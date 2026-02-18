da27c1c77a37691955fe341204873542be5f658c
H.W.K.Aravinda <118424430+Aravinda-HWK@users.noreply.github.com>
2026-02-18 18:36:27 +0530

    Add develop-config.js for runtime configuration

---
+++ thunder/develop-config.js
@@ -0,0 +1,10 @@
+window.__THUNDER_RUNTIME_CONFIG__ = {
+  client: {
+    base: '/develop',
+    client_id: 'DEVELOP',
+    scopes: ['openid', 'profile', 'email', 'system'],
+  },
+  server: {
+    public_url: 'https://<domain_name>:<port>',
+  },
+};
