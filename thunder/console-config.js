2f4445d73b030b7d6bbcc2ba31caa9a3365ff4ef
H.W.K.Aravinda <118424430+Aravinda-HWK@users.noreply.github.com>
2026-04-09 11:09:03 +0530

    Add console-config.js for Thunder runtime configuration

---
+++ thunder/console-config.js
@@ -0,0 +1,10 @@
+window.__THUNDER_RUNTIME_CONFIG__ = {
+  client: {
+    base: '/console',
+    client_id: 'CONSOLE',
+    scopes: ['openid', 'profile', 'email', 'system'],
+  },
+  server: {
+    public_url: 'https://<domain_name>:<port>',
+  },
+};
